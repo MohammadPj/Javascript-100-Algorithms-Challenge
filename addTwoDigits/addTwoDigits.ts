88704fe4f2385084a642327b34995d53745a8aca
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-12 20:29:31 +0430

    addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,5 @@
-export function addTwoDigits(n: any): number {
-
+export function addTwoDigits(n: number): number {
+  return n.toString().split('').reduce((total , current) => total + Number(current), 0)
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
