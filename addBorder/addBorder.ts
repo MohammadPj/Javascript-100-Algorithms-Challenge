0b9772d0250a6a1ef421ef59676cd5e02249ca17
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-12 20:24:11 +0430

    add and border

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,15 @@
 export function addBorder(picture: string[]): string[] {
+  const wallLength = picture[0].length + 2;
+  let wall = "*".repeat(wallLength);
 
+  
+  picture.forEach((p, i) => {
+      picture[i] = '*'.concat(p, '*')
+    })
+    
+    picture.unshift(wall)
+    picture.push(wall)
+
+  return picture;
 }
-
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
