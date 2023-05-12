3fae38ef4606f9a1422e4b503a41a6e8dd4ffcf2
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-12 22:52:07 +0430

    adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,10 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+    let max = 0
+    for (let i = 0; i < inputArray.length; i++) {
+        const res = inputArray[i] * inputArray[i + 1]
+        max = res > max ? res : max
+    }
+    return max
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
