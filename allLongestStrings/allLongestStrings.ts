33a725db4bafd794ce5fd2a52db2bf73dc8e50a5
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-12 23:28:10 +0430

    allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  let longestLength = 0
+  let longestWorlds: string[] = []
 
+  inputArray.forEach(word => {
+    longestLength = word.length > longestLength ? word.length : longestLength
+  })
+  
+  inputArray.forEach(word => {
+    if(word.length === longestLength) {
+        longestWorlds.push(word)
+    }
+  })
+
+  return longestWorlds
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
