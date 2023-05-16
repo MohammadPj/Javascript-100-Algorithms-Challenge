699c67a3c6c21f27464f04d0b952342547b93def
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-16 23:40:02 +0430

    alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,18 @@
 export function alternatingSums(a: number[]): number[] {
+  let team1 = 0
+  let team2 = 0
 
+  for (let i = 0; i < a.length;) {
+    team1 += a[i]
+    i = i + 2
+  }
+
+  for (let i = 1; i < a.length;) {
+    team2 += a[i]
+    i = i + 2
+  }
+
+  return [team1, team2]
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
