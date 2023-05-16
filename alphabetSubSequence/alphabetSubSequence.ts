8fe3fc63fc848225064458471806f006f4e1ec18
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-16 23:25:19 +0430

    alphabetSubsequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,16 @@
 export function alphabetSubsequence(s: string): boolean {
+  const chars: string[] = s.split('')
+  const charValues: number[] = chars.map(c => c.charCodeAt(0))
 
+  if(new Set(charValues).size !== charValues.length) return false;
+  
+  for (let i = 0; i < charValues.length; i++) {
+    if(charValues[i] > charValues[i + 1]) {
+      return false
+    }
+  }
+
+  return true
 }
 
 console.log(alphabetSubsequence('zab'))
