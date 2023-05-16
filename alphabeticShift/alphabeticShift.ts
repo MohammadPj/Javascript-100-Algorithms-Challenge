65bd8e7a0e73511e4292047deb03c396bd87fbab
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-16 22:10:44 +0430

    alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,37 @@
 export function alphabeticShift(inputString: string): string {
+  const alphabet = {
+    a: "b",
+    b: "c",
+    c: "d",
+    d: "e",
+    e: "f",
+    f: "g",
+    g: "h",
+    h: "i",
+    i: "j",
+    j: "k",
+    k: "l",
+    l: "m",
+    m: "n",
+    n: "o",
+    o: "p",
+    p: "q",
+    q: "r",
+    r: "s",
+    s: "t",
+    t: "u",
+    u: "v",
+    v: "w",
+    w: "x",
+    x: "y",
+    y: "z",
+    z: "a",
+  };
 
+  return inputString
+    .split("")
+    .map((l) => alphabet[l])
+    .join("");
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("crazy"));
