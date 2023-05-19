5ba683cdfbbfbc217bd3d8b57a2e6b7348136589
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 16:08:31 +0430

    arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,10 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
 
+  return inputArray.map(e => e === elemToReplace ? substitutionElem : e)
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
