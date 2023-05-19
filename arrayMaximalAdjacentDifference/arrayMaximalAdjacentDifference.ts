7731a12a48e492becb4841c09ee11aba7895cd4f
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 14:43:50 +0430

    arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,12 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let max = 0
+    for (let i = 0; i < inputArray.length; i++) {
+      const element = inputArray[i];
+      const prvElement = inputArray[i - 1] || 0;
 
+      if(Math.abs(element - prvElement) > max) max = Math.abs(element - prvElement)
+    }
+  return max
 }
 
-// console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
+console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
