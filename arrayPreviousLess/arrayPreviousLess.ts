16d932668a47233c2d35686c22b5e3b38284aa08
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 16:03:45 +0430

    arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,20 @@
 export function arrayPreviousLess(items: number[]): number[] {
+  const newArray: number[] = [];
 
+  for (let i = items.length - 1; i >= 0; i--) {
+
+    for (let j = i ; j >= 0; j--) {
+      
+      if (items[j] < items[i]) {
+        newArray.unshift(items[j])
+        break;
+      } else if (j === 0) {
+        newArray.unshift(-1)
+      }
+    }
+  }
+
+  return newArray;
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
