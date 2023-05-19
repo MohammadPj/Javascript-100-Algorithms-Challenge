091f8d9fc5a93322a807afd248069f6f6a76f637
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 20:46:51 +0430

    isCaseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,11 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+  const originalLowerCased = inputString.toLowerCase()
+  const reversedWord = originalLowerCased.split('').reverse().join('')
+  
+  if(originalLowerCased === reversedWord) return true
+  
+  return false
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abaca'));
