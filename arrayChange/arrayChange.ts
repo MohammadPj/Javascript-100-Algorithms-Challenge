4137a99bf0196ec64cd8788ba6623e6033c163ec
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 13:08:24 +0430

    arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,19 @@
 export function arrayChange(inputArray: number[]): number {
+  let moves = 0
 
+    for (let i = 0; i < inputArray.length; i++) {
+      let element = inputArray[i];
+      const prvElement = inputArray[i - 1] || 0
+
+      if(element <= prvElement) {
+        const diff = prvElement - element + 1
+        moves += diff
+        element = prvElement + 1
+        inputArray[i] = element
+      }
+
+    }
+  return moves
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
