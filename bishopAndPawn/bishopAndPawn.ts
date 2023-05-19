20277169442e058335e5734fbe0034e959188aab
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 19:57:41 +0430

    bishopAndPawn

--- bishopAndPawn/bishopAndPawn.ts
+++ bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,19 @@
 export function bishopAndPawn(bishop: string, pawn: string): boolean {
+  const verticals = ["a", "b", "c", "d", "e", "f", "g"];
 
+  let bishopX = verticals.indexOf(bishop.split("")[0]);
+  let bishopY = Number(bishop.split("")[1]) - 1
+  let pawnX = verticals.indexOf(pawn.split("")[0]);
+  let pawnY = Number(pawn.split("")[1]) - 1
+
+  const diff = pawnY - bishopY;
+
+  if (
+    verticals[bishopX - diff] === verticals[pawnX] ||
+    verticals[bishopX + diff] === verticals[pawnX]
+  )
+    return true;
+  return false;
 }
 
-// console.log(bishopAndPawn('a1', 'c3'));
+console.log(bishopAndPawn("d6", "e7"));
