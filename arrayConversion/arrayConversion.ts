66c5136b59a13122fa275988232cefd4c56b0ef1
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 14:31:45 +0430

    arrayMaxConsecutiveSum

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,5 +1,11 @@
 export function arrayConversion(inputArray: number[]): number {
 
+  for (let i = 0; i < inputArray.length / 2 - 1; i++) {
+    const element = inputArray[i];
+    
+  }
+
+  return 0
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,21 @@
-export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+export function arrayMaxConsecutiveSum(
+  inputArray: number[],
+  k: number
+): number {
+  let max = 0;
+  let sum = 0;
 
+  for (let i = 0; i < k; i++) {
+    sum += inputArray[i]
+  }
+
+  for (let i = k; i < inputArray.length; i++) {
+    sum -= inputArray[i - k]
+    sum += inputArray[i]
+
+    if(sum > max) max = sum
+  }
+  return max;
 }
 
-// console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
