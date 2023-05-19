7a20ff977a940ec98a28ec0ab1574d0a2bb99e65
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 20:59:04 +0430

    characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,10 @@
 export function characterParity(symbol: string): string {
 
+  const parsedNjmber = Number(symbol)
+
+ return isNaN(parsedNjmber) ? 'not a digit' : parsedNjmber % 2 === 0 ? 'even' : 'odd'
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'))
+console.log(characterParity('8'))
+console.log(characterParity('q'))
