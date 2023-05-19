2d7193ee78396537b9e5dd37131d5d3f36931007
Mohammad <mohammadpoorjamal@gmail.com>
2023-05-19 18:57:40 +0430

    avoidObstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,15 @@
 export function avoidObstacles(inputArray: number[]): number {
+  const sortedArray = inputArray.sort((a, b) => a - b);
 
+  for (let i = 1; i <= sortedArray[sortedArray.length - 1]; i++) {
+    if (sortedArray[i] !== i) {
+      if (sortedArray.every((item) => item % i !== 0)) {
+        return i
+      }
+    }
+  }
+
+ return sortedArray[sortedArray.length - 1] + 1
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([2,4,6,10, 14]));
