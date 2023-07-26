cdb1d5063e9e68a100798c929a89f86e906c8f08
Muhammad <mohammadpoorjamal@gmail.com>
2023-07-26 11:46:58 +0330

    confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,9 @@
 export function confirmEnding(str: string, target: string) {
 
+  const length = str.length - target.length
+  return str.substring(length, str.length) === target
+  
 }
 
-// console.log(confirmEnding("Abstraction", "action"));
-// console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Abstraction", "action"));
+console.log(confirmEnding("Open sesame", "pen"));
